bc1b6e9053cbd010054105115bd44488db60e8b3
eggvr <92230210+eggvr-png@users.noreply.github.com>
2024-11-03 15:19:17 -0500

    whoopsies i forgor about the sourse code
    sorry :(

---
+++ Plugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace AssetBundleTemplate
+{
+	[ModdedGamemode]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		bool inRoom;
+		public static Plugin instance;
+        public static AssetBundle bundle;
+        public static GameObject assetBundleParent;
+        public static string assetBundleName = "wheatly";
+        public static string parentName = "BundleParent (put objects in here DONT MOVE)";
+
+        void Start()
+		{
+			Utilla.Events.GameInitialized += OnGameInitialized;
+		}
+
+		void OnEnable()
+		{
+
+		}
+
+		void OnDisable()
+		{
+
+		}
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+			instance = this;
+
+			//This loads the asset bundle put in the AssetBundles folder
+			bundle = LoadAssetBundle("GorillaTagAssetBundleModTemplate.AssetBundles." + assetBundleName);
+
+            //Spawn in Parent
+            assetBundleParent = Instantiate(bundle.LoadAsset<GameObject>(parentName));
+
+            //Set Parent Pos (DO NOT CHANGE)
+            assetBundleParent.transform.position = new Vector3(-67.2225f, 11.57f, -82.611f);
+        }
+
+        public AssetBundle LoadAssetBundle(string path)
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+            AssetBundle bundle = AssetBundle.LoadFromStream(stream);
+            stream.Close();
+            return bundle;
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace AssetBundleTemplate
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.eggdogy.gorillatag.wheatlyFromPortal2";
+        public const string Name = "Intelligence Dampening Sphere";
+        public const string Version = "1.0.0";
+    }
+}
